a36a0451c63510cca633fd918781d1a4f3d320c0
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-04-26 22:42:39 -0500

    Still not sowing correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,6 @@ import { createPlan } from "./plan.js";
 import { plantSeeds } from "./tractor.js";
 import { addPlant, usePlants } from "./field.js";
 
-// import { createAsparagus } from "./seeds/asparagus.js";
-// import { asparagusSeed } from "./seeds/asparagus.js";
-
-// import { createCorn } from "./seeds/corn.js";
-// import { cornSeed } from "./seeds/corn.js";
-
-// import { createPotato } from "./seeds/potato.js";
-// import { potatoSeed } from "./seeds/potato.js";
-
-// import { createSoybean } from "./seeds/soybean.js";
-// import { soybeanSeed } from "./seeds/soybean.js";
-
-// import { createSunflower } from "./seeds/sunflower.js";
-// import { sunflowerSeed } from "./seeds/sunflower.js";
-
-// import { createWheat } from "./seeds/wheat.js";
-// import { wheatSeed } from "./seeds/wheat.js";
-
 console.log("Welcome to the main module");
 
 // Chapter 1: Harvest Plan
@@ -36,8 +18,8 @@ const yearlyPlan = createPlan();
 // console.log(currentTilling);
 
 // Chapter 4: Sowing the Field
-plantSeeds(yearlyPlan);
-usePlants();
+// plantSeeds(yearlyPlan);
+// usePlants();
 const planting = plantSeeds(yearlyPlan);
 
 console.log(planting);
